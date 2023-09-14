417a258824665392a80b699f0a13c224c097b290
Justin10612 <atom.9031@gmail.com>
2023-09-15 00:32:55 +0800

    New Resolution

--- detection/main.py
+++ detection/main.py
@@ -13,27 +13,30 @@ class yolov5_ros(Node):
     def __init__(self):
         super().__init__('human_detector')
         self.publisher_ = self.create_publisher(Vector3, 'human_pos', 10)
-        # 
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/sss0301/ros2_ws/src/detection/models/bestv8.pt')
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/sss0301/ros2_ws/src/detection/models/bestv8.pt', force_reload=True)
         # self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
         self.model.conf = 0.5
         
     def dectshow(self, org_img, boxs, depth_data):
         detection_pos = []
+        def clamp(n, smallest, largest):
+            return max(smallest, min(n, largest))
         # The func that can lat U know the distance
         def get_mid_pos(box, depth_data):
             distance_list = []
-            depth_heigh = 300
+            depth_heigh = 200
             bias = 10
-            target_x = (box[0] + box[2])//2    # The center-x of the bounding box
+            target_x = int(clamp((box[0] + box[2])//2, 0, 1280))    # The center-x of the bounding box
             # Get the smaple point
             for i in range(3):
-                distance_list.append(depth_data[int(target_x), int(depth_heigh+bias)])
-                bias +=10
+                target_y = int(clamp(depth_heigh+bias, 0, 720))
+                cv2.circle(org_img, (int(target_x), int(target_y)), 8, (255,255,255), 2)
+                distance_list.append(depth_data[target_y, target_x])
+                bias +=5
+                # self.get_logger().info('Human.angle "%f"' % (target_y))
             # Let you know where the smaple point is.
-            cv2.circle(org_img, (int(target_x), int(depth_heigh)), 8, (255,255,255), 2)
-            distance_list.append(depth_data[int(target_x+bias), int(depth_heigh)])
-            return [target_x, np.mean(distance_list)]
+            # cv2.circle(org_img, (int(target_x), int(depth_heigh)), 8, (255,255,255), 2)
+            return np.mean(distance_list)
         
         # Gvie Every Box Distance_Value
         for box in boxs:
@@ -43,17 +46,22 @@ class yolov5_ros(Node):
             pos = get_mid_pos(box, depth_data)
             # THIS CAN ONLY DEAL WITH SINGLE OBJET
             # Show Name and Distance
-            cv2.putText(org_img, "Target" + str(float(pos[1]) / 1000)[:4] + 'm',
+            cv2.putText(org_img, "Target" + str(float(pos) / 1000)[:4] + 'm',
                         (int(box[0]), int(box[3])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+            # Publish Data
+            # msg = Vector3()
+            # msg.x = pos[0]   # x
+            # msg.y = pos[1]   # depth
+            # self.publisher_.publish(msg)
+            # self.get_logger().info('Human.angle "%f", Human.depth "%f"' % (msg.x, msg.y))
         cv2.imshow('dec_img', org_img)
-        return [pos[0], pos[1]]
     
     def run_detection(self):
         # Configure depth and color streams
         pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 60)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
+        config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
         # Start streaming
         pipeline.start(config)
         try:
@@ -68,6 +76,7 @@ class yolov5_ros(Node):
                 # Convert images to numpy arrays
                 depth_image = np.asanyarray(depth_frame.get_data())
                 color_image = np.asanyarray(color_frame.get_data())
+                # print(np.shape(depth_image))
 
                 results = self.model(color_image)
                 boxs= results.pandas().xyxy[0].values
@@ -82,12 +91,6 @@ class yolov5_ros(Node):
                 # cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
                 # cv2.imshow('RealSense', images)
 
-                # Publish Data
-                msg = Vector3()
-                msg.x = human_pos[0]   # x
-                msg.y = human_pos[1]   # depth
-                self.publisher_.publish(msg)
-                self.get_logger().info('Human.angle "%f", Human.depth "%f"' % (msg.x, msg.y))
                 # Press esc or 'q' to close the image window
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q') or key == 27:
